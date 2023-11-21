5e77e37147f25cdbc81f50c1fadd500c87508c86
haris <haris.laksliosda@gmail.com>
2023-11-21 17:33:20 +0700

    validateForm error

--- js/script.js
+++ js/script.js
@@ -2,5 +2,25 @@ function replaceName() {
     let namaPengunjung = prompt("Silahkan isi dengan nama anda", "");
     document.getElementById("namaPengunjung").innerHTML = namaPengunjung;
 }    
-replaceName();
-console.log(namaPengunjung);
+// replaceName();
+
+function validateForm () {
+    const nama = document.forms["formPesan"]["nama"].value;
+    const email = document.forms["formPesan"]["email"].value;
+    const pesan = document.forms["formPesan"]["pesan"].value;
+
+    if (nama == "" || email == "" || pesan == "") {
+        alert("Data tidak boleh kosong");
+        return false;
+    }
+    setSenderUI(nama, email, pesan);
+    return false;
+}
+// validateForm();
+
+function setSenderUI(nama, email, pesan){
+    document.getElementById("sender-name").innerHTML = nama;
+    document.getElementById("sender-email").innerHTML = email;
+    document.getElementById("sender-pesan").innerHTML = pesan;
+}
+// setSenderUI();
