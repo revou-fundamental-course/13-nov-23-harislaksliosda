d5a88fec4043a765b2bd29474e2b99565bf85801
haris <haris.laksliosda@gmail.com>
2023-11-21 16:33:58 +0700

    add js

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function replaceName() {
+    let namaPengunjung = prompt("Silahkan isi dengan nama anda", "");
+    document.getElementById("namaPengunjung").innerHTML = namaPengunjung;
+}    
+replaceName();
+console.log(namaPengunjung);
