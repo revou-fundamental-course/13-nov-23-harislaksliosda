f958d85889d001c2de67f002166828d310a653e0
haris <haris.laksliosda@gmail.com>
2023-11-22 13:03:52 +0700

    fixing js

--- js/script.js
+++ js/script.js
@@ -5,23 +5,24 @@ function replaceName() {
 // replaceName();
 
 function validateForm () {
-    const name = document.forms["formPesan"]["nama"].value;
-    const email = document.forms["formPesan"]["email"].value;
-    const pesan = document.forms["formPesan"]["pesan"].value;
+    const nama = document.forms["formPesan"]["input-nama"].value
+    const email = document.forms["formPesan"]["email"].value
+    const pesan = document.forms["formPesan"]["pesan"].value
 
-    if (name == "" || email == "" || pesan == "") {
+    if (nama == "" || email == "" || pesan == "") {
         document.getElementById("data-kosong").innerHTML = "Data tidak boleh kosong!"
         // alert("Data tidak boleh kosong");
         return false;
     }
-    setSenderUI(name, email, pesan);
+    setSenderUI(nama, email, pesan);
     return false;
 }
 validateForm();
 
-function setSenderUI(name, email, pesan){
-    document.getElementById("sender-name").innerHTML = name;
+function setSenderUI(nama, email, pesan){
+    document.getElementById("sender-name").innerHTML = nama;
     document.getElementById("sender-email").innerHTML = email;
     document.getElementById("sender-pesan").innerHTML = pesan;
+    document.getElementById("data-kosong").innerHTML = "";  
 }
-// setSenderUI();
+setSenderUI();
