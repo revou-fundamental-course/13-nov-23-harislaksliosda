656bfcab8bf52afbfd52a1e507e6b890dbdd8ce2
haris <haris.laksliosda@gmail.com>
2023-11-22 13:57:16 +0700

    update css

--- js/script.js
+++ js/script.js
@@ -25,4 +25,22 @@ function setSenderUI(nama, email, pesan){
     document.getElementById("sender-pesan").innerHTML = pesan;
     document.getElementById("data-kosong").innerHTML = "";  
 }
-setSenderUI();
+// setSenderUI();
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    if (n > x.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = x.length} ;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[slideIndex-1].style.display = "block";
+}
